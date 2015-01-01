f7828ebf7b3d69d4aab3b2704e1120dc23f8d517
LUO <luo@LUOdeMacBook-Pro.local>
2015-01-01 23:34:51 +0800

    ability to set symbol

--- simple-template.js
+++ simple-template.js
@@ -1,42 +1,61 @@
-function sTmp(tpl, data) {
-	var reg = /<%([^%>]+)?%>/g,
-		code = 'var r = [];',
-		cursor = 0;
+;
+(function() {
+	var frontSymbol = '<%',
+		endSymlbol = '%>';
 
-	function addToCode(piece, isJs) {
-		var _piece = '',
-			regOut = /(^( )?(var|if|for|else|switch|case|break|{|}))(.*)?/g;
+    var sTmp = {};
 
-		if (isJs) {
-			if (regOut.test(piece)) {
-				_piece = piece;
-			} else {
-				_piece = 'r.push('+ piece +');';
-			}
-		} else {
-			_piece = 'r.push("' + piece + '");';
-		}
+    sTmp.setSymbol = function(front, end) {
+    	frontSymbol = front;
+    	endSymlbol = end;
+    };
 
-		if (piece === '') {
-			return;
-		}
+    sTmp.setDefaultSymbol = function() {
+    	frontSymbol = '<%';
+    	endSymlbol = '%>';
+    };
 
-		code += _piece;
-	}
+    sTmp.compile = function(tpl, data) {
+        var reg = new RegExp(frontSymbol + '([^' + endSymlbol + ']+)?' + endSymlbol, "g"),
+            code = 'var r = [];',
+            cursor = 0;
 
-	while(m = reg.exec(tpl)) {
-		addToCode(tpl.slice(cursor, m.index));
-		addToCode(m[1], true);
-		cursor = m.index + m[0].length;
-	}
+        function addToCode(piece, isJs) {
+            var _piece = '',
+                regOut = /(^( )?(var|if|for|else|switch|case|break|{|}))(.*)?/g;
 
-	addToCode(tpl.substr(cursor, tpl.length - cursor));
+            if (isJs) {
+                if (regOut.test(piece)) {
+                    _piece = piece;
+                } else {
+                    _piece = 'r.push(' + piece + ');';
+                }
+            } else {
+                _piece = 'r.push("' + piece + '");';
+            }
 
-	code += 'return r.join("");';
+            if (piece === '') {
+                return;
+            }
 
-	return new Function(code).apply(data);
+            code += _piece;
+        }
 
-}
+        while (m = reg.exec(tpl)) {
+            addToCode(tpl.slice(cursor, m.index));
+            addToCode(m[1], true);
+            cursor = m.index + m[0].length;
+        }
 
-var a = sTmp("<%if (this.a) {%><div><%this.num%></div><%}%>", {a: true, num: 5});
+        addToCode(tpl.substr(cursor, tpl.length - cursor));
+
+        code += 'return r.join("");';
+
+        return new Function(code).apply(data);
+    };
+
+    window.sTmp = sTmp;
+})();
+
+var a = sTmp.compile("<%if (this.a) {%><div><%this.num%></div><%}%>", {a: true, num: 5});
 console.log(a);
